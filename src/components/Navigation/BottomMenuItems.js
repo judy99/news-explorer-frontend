afeea8f5e3a9d681ff96910578ccc556e287d2be
Julia Kurianova <judy@sfdev.com>
2021-01-15 19:50:40 -0500

    Clean code. Restructuring the project

---
+++ src/components/Navigation/BottomMenuItems.js
@@ -0,0 +1,16 @@
+const BottomMenuItems = [
+  {
+    title: 'Home',
+    url: '/',
+    cLink: 'menu__link foo-menu__link',
+    cWrapper: 'menu__item foo-menu__item',
+  },
+  {
+    title: 'Practicum by Yandex',
+    url: 'https://practicum.yandex.com',
+    cLink: 'menu__link foo-menu__link',
+    cWrapper: 'menu__item foo-menu__item',
+  },
+];
+
+export default BottomMenuItems;
